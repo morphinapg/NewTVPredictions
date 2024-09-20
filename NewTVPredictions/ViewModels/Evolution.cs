28b9df9067651ce650b81bdd5ed35eecc86739b3
morphinapg <52674976+morphinapg@users.noreply.github.com>
2024-09-20 04:59:32 -0400

    The NextGeneration step is now able to sort all models in all Evolution models, update top models if necessary, and breed the next generation of children.

--- NewTVPredictions/ViewModels/Evolution.cs
+++ NewTVPredictions/ViewModels/Evolution.cs
@@ -12,11 +12,18 @@ namespace NewTVPredictions.ViewModels
     {
         const int NumberOfModels = 30, NumberOfTrees = 10;
 
+        //All family trees, each with a large number of models
         [DataMember]
         List<PredictionModel>[] FamilyTrees = new List<PredictionModel>[NumberOfTrees];
 
+        //The top performing model currently
+        [DataMember]
+        PredictionModel[] TopModels = new PredictionModel[NumberOfTrees];
+
         [DataMember]
         public Network Network;
+
+        public bool TopModelChanged = false;
 
         /// <summary>
         /// Initialize Evolution model
@@ -27,7 +34,12 @@ namespace NewTVPredictions.ViewModels
             Network = network;
 
             //initialize each family tree
-            Parallel.For(0, NumberOfTrees, i => FamilyTrees[i] = Enumerable.Range(0, NumberOfModels).Select(x => new PredictionModel(Network)).ToList());
+            Parallel.For(0, NumberOfTrees, i =>
+            {
+                FamilyTrees[i] = Enumerable.Range(0, NumberOfModels).Select(x => new PredictionModel(Network)).ToList();
+                TopModels[i] = FamilyTrees[i][0];
+                TopModels[i].TestAccuracy();
+            });
         }
 
         /// <summary>
@@ -45,7 +57,7 @@ namespace NewTVPredictions.ViewModels
         /// <returns></returns>
         public IEnumerable<Func<ErrorContainer>> GetRatingsActions(IEnumerable<WeightedShow> WeightedShows)
         {
-            return FamilyTrees.SelectMany(x => x).Select(x => x.GetRatingsActions(WeightedShows)).SelectMany(x => x);
+            return FamilyTrees.SelectMany(x => x).Where(x => x.Accuracy is null || x.Error is null).Select(x => x.GetRatingsActions(WeightedShows)).SelectMany(x => x);
         }
 
         /// <summary>
@@ -55,7 +67,7 @@ namespace NewTVPredictions.ViewModels
         /// <returns></returns>
         public IEnumerable<Func<ShowErrorContainer>> GetPredictionActions(IEnumerable<WeightedShow> WeightedShows)
         {
-            return FamilyTrees.SelectMany(x => x).Select(x => x.GetPredictionActions(WeightedShows)).SelectMany(x => x);
+            return FamilyTrees.SelectMany(x => x).Where(x => x.Accuracy is null || x.Error is null).Select(x => x.GetPredictionActions(WeightedShows)).SelectMany(x => x);
         }
 
         /// <summary>
@@ -80,6 +92,62 @@ namespace NewTVPredictions.ViewModels
         public override string ToString()
         {
             return Network.Name;
+        }
+
+        /// <summary>
+        /// If the current generation has created a new model that performs better than the current
+        /// top performing model, replace it. Otherwise, replace the first model in the FamilyTree
+        /// with the existing top performing model, for breeding purposes
+        /// </summary>
+        public IEnumerable<Action> UpdateModelActions()
+        {
+            return Enumerable.Range(0, NumberOfTrees).Select(i => new Action(() =>
+            {
+                if (FamilyTrees[i][0] > TopModels[i])
+                {
+                    TopModels[i] = FamilyTrees[i][0];
+                    TopModelChanged = true;
+                }                    
+                else
+                    FamilyTrees[i][0] = TopModels[i];
+            }));
+        }
+
+        /// <summary>
+        /// Get all Actions necessary to breed the existing parent models for a new generation
+        /// </summary>
+        public IEnumerable<Action> GetBreedingActions()
+        {
+            //First, we need to make a temporary copy of all existing family trees
+            var LastGeneration = new List<PredictionModel>[NumberOfTrees];
+            for (int i = 0; i < NumberOfTrees; i++)
+                LastGeneration[i] = FamilyTrees[i].ToList();
+
+            var r = Random.Shared;
+            var Peak = Math.Log10(NumberOfModels + 1);
+
+            return Enumerable.Range(0, NumberOfTrees).Select(x => Enumerable.Range(0, NumberOfModels).Select(y => new { x, y })).SelectMany(x => x).Select(model => new Action(() =>
+            {
+                if (model.y == NumberOfModels - 1)
+                {
+                    //The final model in the next generation should be randomized, to introduce occasional added variation
+                    FamilyTrees[model.x][model.y] = new PredictionModel(Network);
+                }
+                else
+                {
+                    //The rest of the models should be breeded by selecting two parents
+                    //The better performing the mode, the more likely to be chosen as a parent
+                    int
+                        Parent1 = (int)(Math.Pow(10, r.NextDouble() * Peak)) - 1,
+                        Parent2 = (int)(Math.Pow(10, r.NextDouble() * Peak)) - 1;
+
+                    if (Parent1 == Parent2)
+                        FamilyTrees[model.x][model.y] = LastGeneration[model.x][Parent1];
+                    else
+                        FamilyTrees[model.x][model.y] = LastGeneration[model.x][Parent1] + LastGeneration[model.x][Parent2];
+                }
+            }));
+
         }
     }
 }
--- NewTVPredictions/ViewModels/EvolutionController.cs
+++ NewTVPredictions/ViewModels/EvolutionController.cs
@@ -10,13 +10,13 @@ namespace NewTVPredictions.ViewModels
     internal class EvolutionController
     {
 
-        public List<Evolution> AllNetworks;
+        public ParallelQuery<Evolution> AllNetworks;
         public ConcurrentDictionary<Evolution, IEnumerable<WeightedShow>> WeightedShows = new();
         public ConcurrentDictionary<Network, IEnumerable<EpisodePair>> EpisodePairs = new();
 
         public EvolutionController(List<Evolution> allNetworks)
         {
-            AllNetworks = allNetworks;
+            AllNetworks = allNetworks.AsParallel();
             Parallel.ForEach(AllNetworks, x =>
             {
                 WeightedShows[x] = x.GetWeightedShows();
@@ -26,27 +26,42 @@ namespace NewTVPredictions.ViewModels
 
         public void NextGeneration()
         {
-            var ParallelNetworks = AllNetworks.AsParallel();
+            AllNetworks.ForAll(x => x.TopModelChanged = false);
+
 
             // STEP 1 - ACCURACY TESTING //
             //First, we need to test every PredictionModel in every Evolution model for accuracy
 
             //The first step to do that is to run every action needed to test the RatingsModel of every PredictionModel in parallel
             var RatingsResults = new ConcurrentDictionary<PredictionModel, IEnumerable<ErrorContainer>>();
-                
-                ParallelNetworks.Select(x => x.GetRatingsActions(WeightedShows[x])).SelectMany(x => x).Select(x => x())
-                .GroupBy(x => x.Model).ForAll(x => RatingsResults[x.Key] = x.AsEnumerable());
+
+            AllNetworks.Select(x => x.GetRatingsActions(WeightedShows[x])).SelectMany(x => x).Select(x => x())
+            .GroupBy(x => x.Model).ForAll(x => RatingsResults[x.Key] = x.AsEnumerable());
 
             //Next, run all actions needed to test the RenewalModel of every PredictionModel
             var PredictionResults = new ConcurrentDictionary<PredictionModel, IEnumerable<ShowErrorContainer>>();
 
-            ParallelNetworks.Select(x => x.GetPredictionActions(WeightedShows[x])).SelectMany(x => x).Select(x => x())
+            AllNetworks.Select(x => x.GetPredictionActions(WeightedShows[x])).SelectMany(x => x).Select(x => x())
             .GroupBy(x => x.Model).ForAll(x => PredictionResults[x.Key] = x.AsEnumerable());
 
             //Get all PredictionModel objects, and then run the code that sets the accuracy and error for each model
             RatingsResults.AsParallel().Select(x => x.Key).ForAll(x => x.SetAccuracyAndError(RatingsResults[x], PredictionResults[x], EpisodePairs[x.Network]));
 
             // STEP 2 - SORTING //
+            AllNetworks.Select(x => x.GetSortActions()).SelectMany(x => x).ForAll(x => x());
+
+            // STEP 3 - UPDATE TOP MODELS //
+            AllNetworks.Select(x => x.UpdateModelActions()).SelectMany(x => x).ForAll(x => x());
+
+            // STEP 4 - BREEDING //
+            AllNetworks.Select(x => x.GetBreedingActions()).SelectMany(x => x).ForAll(x => x());
+
+
+            // STEP 5 - MUTATION //
+
+
+
+            // STEP 6 - UPDATE EVOLUTION ACCURACY, IF TOP MODELS CHANGED //
         }
     }
 }
